334fbab1a268775c1561207510c52a6c6a27ce6e
Ray_y <ray@ray-y.co.uk>
2025-12-14 19:12:49 +0000

    Changed to GPL-2 license.

--- script/menu.js
+++ script/menu.js
@@ -1,3 +1,9 @@
+//
+// ray-y.co.uk © 2025 Ray Dunn
+//
+// This source code is licensed under the terms of GPL-2.0
+// https://www.gnu.org/licenses/old-licenses/gpl-2.0.en.html/
+
 var toggledOn = false;
 const menu = document.getElementById("menu-items");
 const menuButton = document.getElementById('menuBtn');
