e771908be4a2d7c3a15e9b5d66ddb177d4565262
Ray_y <ray@ray-y.co.uk>
2026-07-05 01:04:04 +0100

    add blabbering page

--- script/global.js
+++ script/global.js
@@ -2,7 +2,7 @@ let localStorageAllowed = localStorage.getItem('consent') === 'true';
 let localStorageAlertShown = false;
 let theme = Number(localStorage.getItem('savedTheme')) || 1;
 
-const pages = ["index.html", "proj", "technologies", "contact"];
+const pages = ["index.html", "proj", "technologies", "contact", "blabbering/"];
 const path = window.location.pathname.split("/").pop();
 const currentFile = path === "" ? "index.html" : path;
 
